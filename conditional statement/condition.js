d8957f74eb482d0e322ed00394038c552eab658c
harinisha-ramesh <131849375+harinisha-ramesh@users.noreply.github.com>
2024-05-28 11:58:57 +0530

    Add files via upload

---
+++ conditional statement/condition.js
@@ -0,0 +1,16 @@
+// If else condition
+let pwd=false
+
+if(pwd)
+    console.log("You are logged in")
+else
+    console.log("Incorrect password")
+
+console.log("Bye") 
+
+//another method
+let a=30, b=50
+max=a<b?a:b //here the [a<b] denotes the symbol to be calculated
+// and question mark has to be givenand a:b a is like if statement and b is else statement
+console.log(max)
+
