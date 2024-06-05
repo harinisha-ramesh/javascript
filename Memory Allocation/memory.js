f1f8ebc15bfafb82ccb8442780de1abbaf9545e3
harinisha-ramesh <131849375+harinisha-ramesh@users.noreply.github.com>
2024-06-05 10:39:27 +0530

    Add files via upload

---
+++ Memory Allocation/memory.js
@@ -0,0 +1,11 @@
+//Memory Allocation
+/*In memory there are 2 segments they are  
+        i) Heap segment
+        ii)Stack segment
+*/
+// Primitive data types will be stored only in stack
+// Refernce types will be stored only in heap
+/* after storing the refernce type in the heap
+   the heap will generate a pointer where the address of the refernce type which is stored in heap
+   That pointer will be stored in stack */
+   
